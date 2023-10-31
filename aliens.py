f80ba78a18dfd8fb3c8dd236c5d995bf8112b390
isayama <c0a22159da@edu.teu.ac.jp>
2023-10-31 17:26:59 +0900

    追加機能敵追加

--- aliens.py
+++ aliens.py
@@ -35,12 +35,13 @@ if not pg.image.get_extended():
 
 
 # game constants
-MAX_SHOTS = 2  # most player bullets onscreen
+MAX_SHOTS = 4  # most player bullets onscreen
 ALIEN_ODDS = 22  # chances a new alien appears
 BOMB_ODDS = 60  # chances a new bomb will drop
 ALIEN_RELOAD = 12  # frames between new aliens
 SCREENRECT = pg.Rect(0, 0, 640, 480)
 SCORE = 0
+Pachi = False
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 
@@ -133,6 +134,29 @@ class Alien(pg.sprite.Sprite):
         self.frame = self.frame + 1
         self.image = self.images[self.frame // self.animcycle % 3]
 
+
+class Add_Enemy(pg.sprite.Sprite):
+    """An alien space ship. That slowly moves down the screen."""
+
+    speed = 150
+    animcycle = 10
+
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self, self.containers)
+        self.image = pg.transform.scale(self.image, (30, 30))
+        self.rect = self.image.get_rect()
+        self.facing = random.choice((-1, 1)) * Add_Enemy.speed
+        self.frame = 0
+        if self.facing < 0:
+            self.rect.right = SCREENRECT.right
+
+    def update(self):
+        self.rect.move_ip(self.facing, 0)
+        if not SCREENRECT.contains(self.rect):
+            self.facing = -self.facing
+            self.rect.top = self.rect.bottom + 1
+            self.rect = self.rect.clamp(SCREENRECT)
+        
 
 class Explosion(pg.sprite.Sprite):
     """An explosion. Hopefully the Alien and not the player!"""
@@ -229,6 +253,7 @@ class Score(pg.sprite.Sprite):
 
 
 def main(winstyle=0):
+
     # Initialize pygame
     if pg.get_sdl_version()[0] == 2:
         pg.mixer.pre_init(44100, 32, 2, 1024)
@@ -250,6 +275,7 @@ def main(winstyle=0):
     img = load_image("explosion1.gif")
     Explosion.images = [img, pg.transform.flip(img, 1, 1)]
     Alien.images = [load_image(im) for im in ("alien1.gif", "alien2.gif", "alien3.gif")]
+    Add_Enemy.image = load_image("chimp.png")
     Bomb.images = [load_image("bomb.gif")]
     Shot.images = [load_image("shot.gif")]
 
@@ -277,6 +303,7 @@ def main(winstyle=0):
 
     # Initialize Game Groups
     aliens = pg.sprite.Group()
+    addenemys = pg.sprite.Group()
     shots = pg.sprite.Group()
     bombs = pg.sprite.Group()
     all = pg.sprite.RenderUpdates()
@@ -285,6 +312,7 @@ def main(winstyle=0):
     # assign default groups to each sprite class
     Player.containers = all
     Alien.containers = aliens, all, lastalien
+    Add_Enemy.containers = addenemys, all, lastalien
     Shot.containers = shots, all
     Bomb.containers = bombs, all
     Explosion.containers = all
@@ -355,6 +383,15 @@ def main(winstyle=0):
             Alien()
             alienreload = ALIEN_RELOAD
 
+        global Pachi
+        #敵追加
+        if SCORE > 0:
+            if  SCORE % 20 == 0 and Pachi == False:
+                Add_Enemy()
+                Pachi = True
+            if SCORE % 20 != 0:
+                Pachi = False
+
         # Drop bombs
         if lastalien and not int(random.random() * BOMB_ODDS):
             Bomb(lastalien.sprite)
@@ -368,12 +405,29 @@ def main(winstyle=0):
             SCORE = SCORE + 1
             player.kill()
 
+        # Detect collisions between aliens and players.
+        for addenemy in pg.sprite.spritecollide(player, addenemys, 1):
+            if pg.mixer:
+                boom_sound.play()
+            Explosion(alien)
+            Explosion(player)
+            SCORE = SCORE + 1
+            player.kill()
+
         # See if shots hit the aliens.
         for alien in pg.sprite.groupcollide(aliens, shots, 1, 1).keys():
             if pg.mixer:
                 boom_sound.play()
             Explosion(alien)
             SCORE = SCORE + 1
+
+        # See if shots hit the addenemy(ぱっちぃ).
+        for addenemy in pg.sprite.groupcollide(addenemys, shots, 1, 1).keys():
+            if pg.mixer:
+                boom_sound.play()
+                Explosion(addenemy)
+                SCORE = SCORE + 10
+            
 
         # See if alien boms hit the player.
         for bomb in pg.sprite.spritecollide(player, bombs, 1):
